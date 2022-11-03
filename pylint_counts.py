603b1d29cff3b102677d9b42d127e567952d218f
Simon Shaw <SJShaw@users.noreply.github.com>
2022-11-03 16:28:16 +0100

    pylint_counts: handle newer pylint versions not using dictionaries

--- pylint_counts.py
+++ pylint_counts.py
@@ -13,11 +13,22 @@ pylint_params = ["-j", "12",]
 output = StringIO()
 reporter = TextReporter(output=output)
 result = Run(pylint_params + paths_to_check, reporter=reporter, do_exit=False, exit=False)
-simple = {
-    "Errors": result.linter.stats["error"],
-    "Convention": result.linter.stats["convention"],
-    "Refactor": result.linter.stats["refactor"],
-    "Warnings": result.linter.stats["warning"],
-    "Syntax/Fatal errors": result.linter.stats["fatal"],
-}
+
+# somewhere between 2.10 and 2.15, pylint stopped using a dictionary, so handle both cases
+if isinstance(result.linter.stats, dict):  # pylint <= 2.10
+    simple = {
+        "Errors": result.linter.stats["error"],
+        "Convention": result.linter.stats["convention"],
+        "Refactor": result.linter.stats["refactor"],
+        "Warnings": result.linter.stats["warning"],
+        "Syntax/Fatal errors": result.linter.stats["fatal"],
+    }
+else:  # pylint >= 2.15
+    simple = {
+        "Errors": result.linter.stats.error,
+        "Convention": result.linter.stats.convention,
+        "Refactor": result.linter.stats.refactor,
+        "Warnings": result.linter.stats.warning,
+        "Syntax/Fatal errors": result.linter.stats.fatal,
+    }
 print(json.dumps(simple))
