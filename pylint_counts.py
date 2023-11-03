a63ab802eef81438feb24be1ba3fbb307a1c654b
Simon Shaw <SJShaw@users.noreply.github.com>
2023-11-03 14:58:22 +0100

    pylint_counts: handle version differences in pylint arguments

--- pylint_counts.py
+++ pylint_counts.py
@@ -2,6 +2,7 @@
 
 from io import StringIO
 import json
+from importlib.metadata import version
 import sys
 
 from pylint.lint import Run
@@ -12,7 +13,21 @@ pylint_params = ["-j", "12",]
 
 output = StringIO()
 reporter = TextReporter(output=output)
-result = Run(pylint_params + paths_to_check, reporter=reporter, do_exit=False, exit=False)
+pylint_kwargs = {
+    "reporter": reporter,
+}
+
+try:
+    pylint_version = tuple(int(part) for part in version("pylint").split("."))
+    if pylint_version >= (2, 5, 1):
+        pylint_kwargs["exit"] = False
+    else:
+        pylint_kwargs["do_exit"] = False
+except Exception:
+    # unknown version, default to pre-2.5.1
+    pylint_kwargs["do_exit"] = False
+
+result = Run(pylint_params + paths_to_check, **pylint_kwargs)
 
 # somewhere between 2.10 and 2.15, pylint stopped using a dictionary, so handle both cases
 if isinstance(result.linter.stats, dict):  # pylint <= 2.10
