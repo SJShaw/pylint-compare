9a5de036cd85bae1350202f4fe6243c2f68bf795
Simon Shaw <SJShaw@users.noreply.github.com>
2025-11-03 17:20:27 +0100

    pylint_counts: handle 3.14's changes to multiprocessing and parallel pylint
    Python 3.14 changed the default start method for subprocesses.
    With that, running pylint with threads caused a crash when pylint's
    Run() was at the top level.
    Moving it into a function with a __name__ check resolves that crash.

--- pylint_counts.py
+++ pylint_counts.py
@@ -8,42 +8,57 @@ import sys
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
 
-paths_to_check = sys.argv[1:]
-pylint_params = ["-j", "12",]
+PYLINT_PARAMS = [
+    "-j", "12",
+]
 
-output = StringIO()
-reporter = TextReporter(output=output)
-pylint_kwargs = {
-    "reporter": reporter,
-}
 
-try:
-    pylint_version = tuple(int(part) for part in version("pylint").split("."))
-    if pylint_version >= (2, 5, 1):
-        pylint_kwargs["exit"] = False
-    else:
+def run_pylint(paths_to_check: list[str]) -> Run:
+    output = StringIO()
+    reporter = TextReporter(output=output)
+    pylint_kwargs = {
+        "reporter": reporter,
+    }
+    try:
+        pylint_version = tuple(int(part) for part in version("pylint").split("."))
+        if pylint_version >= (2, 5, 1):
+            pylint_kwargs["exit"] = False
+        else:
+            pylint_kwargs["do_exit"] = False
+    except Exception:
+        # unknown version, default to pre-2.5.1
         pylint_kwargs["do_exit"] = False
-except Exception:
-    # unknown version, default to pre-2.5.1
-    pylint_kwargs["do_exit"] = False
 
-result = Run(pylint_params + paths_to_check, **pylint_kwargs)
+    return Run(PYLINT_PARAMS + paths_to_check, **pylint_kwargs)
 
-# somewhere between 2.10 and 2.15, pylint stopped using a dictionary, so handle both cases
-if isinstance(result.linter.stats, dict):  # pylint <= 2.10
-    simple = {
-        "Errors": result.linter.stats["error"],
-        "Convention": result.linter.stats["convention"],
-        "Refactor": result.linter.stats["refactor"],
-        "Warnings": result.linter.stats["warning"],
-        "Syntax/Fatal errors": result.linter.stats["fatal"],
-    }
-else:  # pylint >= 2.15
-    simple = {
-        "Errors": result.linter.stats.error,
-        "Convention": result.linter.stats.convention,
-        "Refactor": result.linter.stats.refactor,
-        "Warnings": result.linter.stats.warning,
-        "Syntax/Fatal errors": result.linter.stats.fatal,
-    }
-print(json.dumps(simple))
+
+def get_summary(result: Run) -> None:
+    # somewhere between 2.10 and 2.15, pylint stopped using a dictionary, so handle both cases
+    if isinstance(result.linter.stats, dict):  # pylint <= 2.10
+        simple = {
+            "Errors": result.linter.stats["error"],
+            "Convention": result.linter.stats["convention"],
+            "Refactor": result.linter.stats["refactor"],
+            "Warnings": result.linter.stats["warning"],
+            "Syntax/Fatal errors": result.linter.stats["fatal"],
+        }
+    else:  # pylint >= 2.15
+        simple = {
+            "Errors": result.linter.stats.error,
+            "Convention": result.linter.stats.convention,
+            "Refactor": result.linter.stats.refactor,
+            "Warnings": result.linter.stats.warning,
+            "Syntax/Fatal errors": result.linter.stats.fatal,
+        }
+    return simple
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: no paths provided", file=sys.stderr)
+        sys.exit(1)
+    try:
+        print(json.dumps(get_summary(run_pylint(sys.argv[1:]))))
+    except ImportError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
